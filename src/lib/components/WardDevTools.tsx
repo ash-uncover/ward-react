0a31b4463862776b6836bc18d71d3cbbad733ea4
Antoine MAILLARD <ash.uncover@gmail.com>
2023-01-14 13:29:41 +0100

    add files

---
+++ src/lib/components/WardDevTools.tsx
@@ -0,0 +1,51 @@
+import React, {
+  useState
+} from 'react'
+
+import './WardDevTools.css'
+
+export interface WardDevToolsProperties {
+}
+
+export const WardDevTools = ({
+}: WardDevToolsProperties) => {
+
+  // Hooks //
+
+  const [open, setOpen] = useState(false)
+
+  // How to se it ? check docs
+  //useContext({})
+
+  // Events //
+
+  const handleToggleOpen = () => {
+    setOpen(!open)
+  }
+
+  // Rendering //
+
+  const classes = ['ward-dev-tools']
+  if (open) {
+    classes.push('ward-dev-tools--open')
+  }
+
+  return (
+    <div
+      className={classes.join(' ')}
+    >
+
+      <button
+        className='ward-dev-tools__button'
+        onClick={handleToggleOpen}
+      >
+        W
+      </button>
+
+      <div className='ward-dev-tools__dialog'>
+        dialog
+      </div>
+    </div>
+
+  )
+}
